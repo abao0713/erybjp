faf0ed72c662ab53dd49d543d69e519a2f57dd9c
yuanbaojun <yuanbaojun@codemao.cn>
2021-05-18 21:47:37 +0800

    修改層級

---
+++ uiplatform/utils/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-# 
+#-------------------------------------------------------------------------------
+# Name:         __init__.py.py
+# Description:  
+# Author:       yuanbaojun
+# Date:         2021/5/18
+#----------------------------
+
+from uiplatform.utils.common import BaseAssert
+from uiplatform.utils.common import BrowserPage
+from uiplatform.utils.common import Element
+from uiplatform.utils.common import Page
+from uiplatform.utils.logicobject.Baidudupage import BaiduPage
+from uiplatform.utils.data.config_object import Browser
+
+
