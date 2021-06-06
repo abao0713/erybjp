e97073cb9003ad1d7698a11e51f4d77c80edf017
pujen <305634841@qq.com>
2021-06-06 17:13:29 +0800

    修改依赖配置文件

---
+++ manage.py
@@ -0,0 +1,41 @@
+# --------------
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/09/30 11:26
+# @project_name : Platform
+# @author :	yuanbaojun
+# ------------
+
+from create_app import create_app
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from extensions import db
+from uiplatform.models.elemodel import UielementInfo,Uicaseinfo,Uiresultinfo
+# from interface_platform.routes import app_user
+
+app = create_app("development")
+# app.register_blueprint(app_user, url_prefix='/user')
+manager = Manager(app)
+migrate = Migrate(app=app, db=db)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
+"""终端运行1、 E:\Auto_test\EasyTestPujen>python3 manage.py db init
+命令
+2、 E:\Auto_test\EasyTestPujen>python3 manage.py db migrate --message "initial migration"
+python3 manage.py db migrate --message "initial migration"
+3、 E:\Auto_test\EasyTestPujen>python3 manage.py db upgrade
+
+"""
+
+
+
+
+
+
+
+
