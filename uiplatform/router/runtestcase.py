efa30328b5f26d96cb7d4008dac9e4a66a28ec61
pujen <305634841@qq.com>
2021-06-14 20:45:57 +0800

    1、自动生成代码调试 2、开始分布式线程12

--- uiplatform/router/runtestcase.py
+++ uiplatform/router/runtestcase.py
@@ -22,7 +22,7 @@ cur_dir = os.path.dirname(__file__).split('server')[0]
 def run_only_test():
     session_id= uuid.uuid1()
     pares = f"--seid={session_id}"
-    lista = ["-n auto", "uiplatform/utils/business/cycle_check/test_check_web.py::TestHinfo"]
+    lista = ["-n 3", "uiplatform/utils/business/cycle_check/test_check_web.py::TestHinfo"]
     lista.append(pares)
     pytest.main(lista)
     return jsonify(code=200, msg="ok", data={"session_id":session_id})
