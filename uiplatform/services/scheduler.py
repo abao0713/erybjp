f08234535036c3ee87d3f32a6434991503e7a2d5
pujen <305634841@qq.com>
2021-06-19 16:23:46 +0800

    服务器调试12

--- uiplatform/services/scheduler.py
+++ uiplatform/services/scheduler.py
@@ -5,6 +5,8 @@
 # Author:       yuanbaojun
 # Date:         2021/5/21
 #----------------------------
+import socket
+
 import pytest
 
 from uiplatform.utils.common.BaseLoggers import logger
@@ -13,3 +15,6 @@ from uiplatform.utils.common.BaseLoggers import logger
 def h5check_job():
     pytest.main(["uiplatform/utils/business/cycle_check/test_check_web.py"])
     logger.info("定时任务启动")
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print(ip)
