0846ac57e34482236006466ca9b2cb3bd2475681
yuanbaojun <yuanbaojun@codemao.cn>
2021-07-22 14:23:54 +0800

    修改模型数据类

--- uiplatform/utils/common/BaseLoggers.py
+++ uiplatform/utils/common/BaseLoggers.py
@@ -26,9 +26,9 @@ class Log(object):
         resultPath = os.path.join(basedir, "logs")
         if not os.path.exists(resultPath):
             os.mkdir(resultPath)
-        logPath = os.path.join(resultPath, str(datetime.now().strftime("%Y%m%d%H%M")))
-        if not os.path.exists(logPath):
-            os.mkdir(logPath)
+        # logPath = os.path.join(resultPath, str(datetime.now().strftime("%Y%m%d%H%M")))
+        # if not os.path.exists(logPath):
+        #     os.mkdir(logPath)
 
         # 创建一个logger
         self.logger = logging.getLogger(logger)
